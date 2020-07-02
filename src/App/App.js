9dc0117c7349ce7e5851c93ce079e872ceb2d976
Zachary Langley <zaclangley04@gmail.com>
2020-07-02 15:47:49 -0600

    app nav bar init

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <AppLayout>
+      <AppBar/>
+      Welcome to my Cryptocurrency Tracker
+    </AppLayout>
   );
 }
 
