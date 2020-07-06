eeb317e845a9582d570cf88c5906c9219b38fe1a
Zachary Langley <zaclangley04@gmail.com>
2020-07-06 08:53:34 -0600

    add coin grid component

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -2,12 +2,14 @@ import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
 
 export default function() {
     return (
         <Page name={'settings'}>
-            <ConfirmButton/>
             <WelcomeMessage />
+            <ConfirmButton />
+            <CoinGrid />
         </Page>
     )
 }
