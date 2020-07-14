3cb887a4bfde6f1975112dc09cf5daf16671f929
Zachary Langley <zaclangley04@gmail.com>
2020-07-14 15:41:50 -0600

    highcharts config

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -3,6 +3,9 @@ import ReactHighCharts from 'react-highcharts';
 import highchartsConfig from './HighchartsConfig';
 import {Tile} from '../Shared/Tile';
 import {AppContext} from '../App/AppProvider';
+import HighchartsTheme from './HighChartsTheme';
+
+ReactHighCharts.Highcharts.setOptions(HighchartsTheme)
 
 export default function() {
     return (
